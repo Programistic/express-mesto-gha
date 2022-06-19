9e6fe8ba45b619cfc36c64bad272b861dc9204f4
Maksim Dav <maksim.dav@mail.ru>
2022-06-19 08:30:21 +0800

    connect to MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+//const path = require('path');
 const app = express(); // создать express-приложение
 const { PORT = 3000 } = process.env;
 
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true
+});
+
+//app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}`);
-})
+});
