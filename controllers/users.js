86438f675d55f84e366408c524c292900c880697
Maksim Dav <maksim.dav@mail.ru>
2022-06-19 23:23:12 +0800

    fix .catch

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@
 
 const User = require('../models/user');
 
+/*
 module.exports.getAllUsers = (req, res) => {
   User.find({})
     .then(user => res.send({ data: user }))
@@ -13,11 +14,17 @@ module.exports.getUserById = (req, res) => {
     .then(user => res.send({ data: user }))
     .catch(() => res.status(500).send({ message: 'Ошибка при получении профиля по id' }));
 };
+*/
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body; // получаем из объекта запроса данные пользователя, их надо записать в базу
 
   User.create({ name, about, avatar }) // создаём документ на основе пришедших данных
-    .then(user => res.send({ data: user })) // возвращаем клиенту данные
-    .catch(() => res.status(500).send({ message: 'Ошибка при создании профиля' })); // возвращаем клиенту ошибку
+    .then(user => res.status(200).send({ data: user })) // возвращаем клиенту данные
+    .catch((err) => {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        res.status(400).send({ message: 'Некорректные данные!' })
+      }
+      res.status(500).send({ message: 'Ошибка при создании профиля!' })
+    }); // возвращаем клиенту ошибку
 };
