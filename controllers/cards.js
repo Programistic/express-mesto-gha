1b42a24f42cdba4ea24a890688c25ecda9d750f6
Maksim Dav <maksim.dav@mail.ru>
2022-06-23 23:16:10 +0800

    fix card model

--- controllers/cards.js
+++ controllers/cards.js
@@ -15,7 +15,7 @@ module.exports.createCard = (req, res) => {
   const owner = req.user._id;
 
   Card.create({ name, link, owner })
-    .then(card => res.send({ card }))
+    .then(card => res.status(200).send({ card }))
     .catch((err) => {
       handleError(err, res);
     });
