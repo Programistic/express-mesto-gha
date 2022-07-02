8e32488be3e9a2763a65cf61b2d3f891b3b2c377
Maksim Dav <maksim.dav@mail.ru>
2022-07-02 22:57:40 +0800

    add routes for login and authorize

--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,14 @@ const {
   getUserById,
   getUserByIdAndUpdate,
   getUserByIdAndUpdateAvatar,
+  login,
 } = require('../controllers/users');
 
 userRouter.get('/', getAllUsers);
-userRouter.post('/', createUser);
 userRouter.get('/:_id', getUserById);
 userRouter.patch('/me', getUserByIdAndUpdate);
 userRouter.patch('/me/avatar', getUserByIdAndUpdateAvatar);
+userRouter.post('./signin', login);
+userRouter.post('/signup', createUser);
 
 module.exports = userRouter;
