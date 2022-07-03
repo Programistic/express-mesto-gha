fc9becf7ea79c421ea7ba7eff430ce8e25a26294
Maksim Dav <maksim.dav@mail.ru>
2022-07-03 13:49:52 +0800

    fix user.js

--- models/user.js
+++ models/user.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
     validate: {
       validator: (v) => isURL(v),
       message: 'Неверный формат ссылки!',
-    }
+    },
   },
   email: {
     type: String,
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema({
     validate: {
       validator: (v) => isEmail(v),
       message: 'Неверный формат почты!',
-    }
+    },
   },
   password: {
     type: String,
