9fcc6bcf6ccd96b056d0e55785cfd7a4da1367a9
Maksim Dav <maksim.dav@mail.ru>
2022-07-10 19:48:42 +0800

    fix error

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ const {
 } = require('../controllers/users');
 
 userRouter.get('/', getAllUsers);
-userRouter.get('./me', getCurrentUser);
+userRouter.get('/me', getCurrentUser);
 userRouter.get(
   '/:_id',
   celebrate({
