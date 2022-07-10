9b6ae8749546d7c8e8984c605ef0be84efe8984d
Maksim Dav <maksim.dav@mail.ru>
2022-07-10 11:35:20 +0800

    create classes to handle errors

--- app.js
+++ app.js
@@ -58,6 +58,11 @@ app.use('/cards', cardsRouter);
 
 app.use(errors());
 
+app.use((err, req, res, next) => {
+  res.status(err.statusCode).send({ message: err.message });
+  next();
+});
+
 app.use((req, res, next) => {
   res.status(NOT_FOUND).send({ message: 'Ресурс не найден!' });
   next();
